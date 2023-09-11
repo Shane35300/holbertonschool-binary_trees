4eb825203f7ef1a8092f6493e8c71e63238be926
letihab <letihabib1822@gmail.com>
2023-09-11 11:53:58 +0200

    update file O binary

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,28 +13,14 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
+	{
 	return (NULL);
-
+	exit(exit-failure);
+	}
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
-	if (parent != NULL)
-	{
-		new_node->parent = parent;
-
-		if (value < parent->n)
-		{
-			parent->left = new_node;
-		}
-		else if (value > parent->n)
-		{
-			parent->right = new_node;
-		}
-	}
-	if (parent == NULL)
-	{
-		new_node->parent = NULL;
-	}
+	new_node->parent = NULL;
+	
 	return (new_node);
 }

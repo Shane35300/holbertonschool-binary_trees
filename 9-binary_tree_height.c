79edb1f6d30eb47b7c72d32cfabaadca0b4a909a
letihab <letihabib1822@gmail.com>
2023-09-12 12:30:33 +0200

    update file binary height tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height;
 	size_t height = 1;
 
-if (tree == NULL)
+	if (tree == NULL)
 	return (0);
 
 	left_height = binary_tree_height(tree->left);

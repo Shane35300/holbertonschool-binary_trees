76c7f44b1b093cfd2752f5322298ce7f87bd29d5
shane <6874@holbertonstudents.com>
2023-09-12 12:09:56 +0200

    pull changements validing task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,36 +1,32 @@
 #include "binary_trees.h"
-#include <stdio.h>
-
-size_t calculate_tree_height(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	size_t left_height = calculate_tree_height(tree->left);
-	size_t right_height = calculate_tree_height(tree->right);
-
-	return (left_height > right_height ? left_height + 1 : right_height + 1);
-}
-
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: pointer on parent's adress
+ *
+ * Return: measures the height
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	const binary_tree_t *ptr = tree; // Utilisation const pour ptr
-	size_t total_height;
-	size_t i = 0;
-
+	size_t left_height = 0;
+	size_t right_height = 0;
+	size_t height = 1;
 	if (tree == NULL)
 		return (0);
-
-	while (ptr->parent != NULL)
-		ptr = ptr->parent;
-
-	total_height = calculate_tree_height(ptr);
-
-	while (tree->parent != NULL)
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (tree->left != NULL && tree->right != NULL)
 	{
-		tree = tree->parent;
-		i++;
+		if (left_height > right_height)
+		{
+			height = left_height + 1;
+		}
+		else
+		{
+			height = right_height + 1;
+		}
 	}
-
-	return ((total_height - 1) - i);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	return (height);
 }

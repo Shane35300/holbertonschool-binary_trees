62f30f43310dea5cda81f64b251804f83dd65ad7
letihab <letihabib1822@gmail.com>
2023-09-12 12:25:06 +0200

    add betty requirement

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,21 +2,21 @@
 /**
 *binary_tree_height - function that mesure the height of a binary tree
  * @tree : binary tree to be mesure
- * return: always sucess 
+ * Return: height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0;
 	size_t right_height = 0;
 	size_t height = 1;
-if (tree == NULL) 
-        return 0;
-    else {
 
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
-	}
-	if (tree->left != NULL && tree->right != NULL )
+if (tree == NULL)
+	return (0);
+
+	left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+
+	if (tree->left != NULL && tree->right != NULL)
 	{
 		if (left_height > right_height)
 		{
@@ -29,5 +29,6 @@ if (tree == NULL)
 	}
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
+
 	return (height);
 }

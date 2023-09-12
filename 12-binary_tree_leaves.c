af37d2cfda1ef18e460dcaf9a8684c43596c26fb
shane <6874@holbertonstudents.com>
2023-09-12 13:47:20 +0200

    betty correction for task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,22 +1,26 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_leaves - name of the function
+ * Description: count the leaves
+ * @tree: tree
+ * Return: return the number of leaves
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t leaves_left = 0;
-    size_t leaves_right = 0;
-    size_t leaves;
+size_t leaves_left = 0;
+size_t leaves_right = 0;
+size_t leaves;
 
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+if (tree->left == NULL && tree->right == NULL)
+return (1);
 
-    leaves_left = binary_tree_leaves(tree->left);
-    leaves_right = binary_tree_leaves(tree->right);
+leaves_left = binary_tree_leaves(tree->left);
+leaves_right = binary_tree_leaves(tree->right);
 
-    leaves = leaves_left + leaves_right;
+leaves = leaves_left + leaves_right;
 
-    return (leaves);
-
+return (leaves);
 }

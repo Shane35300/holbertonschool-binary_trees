fa1f3f8794774764cfdd6e4a1067feb0bb0a79f3
shane <6874@holbertonstudents.com>
2023-09-13 11:08:17 +0200

    adding the function of the task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,34 +1,39 @@
 #include "binary_trees.h"
 /**
+ *binary_tree_height - function that mesure the height of a binary tree
+ * @tree : binary tree to be mesure
+ * Return: height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	/* La hauteur maximale est le maximum des hauteurs des sous-arbres plus 1 */
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
  * binary_tree_balance - function that mesure the height of a binary tree
  * @tree : binary tree to be mesure
  * Return: the factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = 1;
-	int right_height = 1;
+	int left_height;
+	int right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_balance(tree->left);
-	right_height = binary_tree_balance(tree->right);
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
 
-	if (tree->left != NULL && tree->right != NULL)
-	{
-		if (left_height >= right_height)
-		{
-			printf("%d", left_height);
-			left_height = left_height + 1;
-		}
-		else if (left_height <= right_height)
-		{
-			right_height = right_height + 1;
-		}
-	}
-	if (tree->left == NULL && tree->right == NULL)
-		return (2);
-
-	return (right_height - right_height);
+	return (left_height - right_height);
 }
